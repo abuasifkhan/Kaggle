5d159b1c0a316c15413f05d4459ba650bd9e4eed
Abu Asif Khan Chowdhury <abuasifkhan+github@gmail.com>
2017-05-23 18:58:24 +0600

    Update titanic_svm.py

--- Titanic/titanic_svm.py
+++ Titanic/titanic_svm.py
@@ -8,82 +8,85 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('train.csv')
 
-# Drop useless tables
+# Drop useless columns
 del dataset['Name']
-del dataset['Cabin']
 del dataset['Ticket']
-
-# Get Trainset
-X = dataset.iloc[:, range(2,len(dataset.columns))].values
-y = dataset.iloc[:, 1].values
+del dataset['Cabin']
 
 
-# Taking care of missing data
-from sklearn.preprocessing import Imputer
-ageImputerTrain = Imputer(missing_values=np.nan,strategy = 'mean', axis=0)
-X[:,2] = ageImputerTrain.fit_transform(X[:, 2].reshape(-1,1)).flatten()
+# Replace all the missing values
+dataset['Age'] = dataset['Age'].fillna(dataset['Age'].median())
+dataset['Embarked'] = dataset['Embarked'].fillna('S')
 
+# Importing testset
+testset = pd.read_csv('test.csv')
+
+# Drop useless columns
+del testset['Name']
+del testset['Ticket']
+del testset['Cabin']
+
+# Replace all the missing values
+testset['Age'] = testset['Age'].fillna(testset['Age'].median())
+testset['Embarked'] = testset['Embarked'].fillna('S')
+
+# Get test data
+X_test = testset.iloc[:,1:-1]
 
 # Encoding categorical data
 # Encoding the Independent Variable
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-sexEncoder = LabelEncoder()
-X[:,1] = sexEncoder.fit_transform(X[:,1])
+from sklearn.preprocessing import LabelEncoder
+embarkedLabelEncoder = LabelEncoder()
+embarkedLabelEncoder.fit(pd.concat([dataset['Embarked'],testset['Embarked']] ))
+dataset['Embarked'] = embarkedLabelEncoder.transform(dataset['Embarked'])
+testset['Embarked'] = embarkedLabelEncoder.transform(testset['Embarked'])
 
-embarkedEncoder = LabelEncoder()
-X[:,6] = embarkedEncoder.fit_transform(X[:,6])
+sexLabelEncoder = LabelEncoder()
+sexLabelEncoder.fit(pd.concat( [dataset['Sex'],testset['Sex']] ) )
+dataset['Sex'] = sexLabelEncoder.transform(dataset['Sex'])
+testset['Sex'] = sexLabelEncoder.transform(testset['Sex'])
 
+# The columns we'll use to predict the target
+predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
 
-## Feature Scaling
-#from sklearn.preprocessing import StandardScaler
-#ageScale = StandardScaler()
-#X[:,2] = ageScale.fit_transform(X[:,2].reshape(-1,1)).flatten()
+# Get data
+X = dataset.iloc[:,2:-1]
+y = dataset.iloc[:,1]
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test_, y_train, y_test_ = train_test_split(X, y, test_size = 0.25, random_state = 0)
+## Splitting the dataset into the Training set and Test set
+#from sklearn.cross_validation import train_test_split
+#X_train, X_test_, y_train, y_test_ = train_test_split(X, y, test_size = 0.0, random_state = 0)
 
+X_train = X
+y_train = y
 
 # Fitting Kernel SVM to the Training set
 from sklearn.svm import SVC
 classifier = SVC(kernel = 'rbf', random_state = 0)
 classifier.fit(X_train, y_train)
 
-# Predicting the Test set results
-y_pred_ = classifier.predict(X_test_)
-
-# Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test_, y_pred_)
+## Predicting the Test set results
+#y_pred = classifier.predict(X_test_)
 
 
 
-'''# Importing the test dataset
-testset = pd.read_csv('test.csv')
-
-
-# Drop useless tables
-del testset['Name']
-del testset['Cabin']
-del testset['Ticket']
-
-# Get testset
-X_test = testset.iloc[:,range(1,len(testset.columns))].values
-
-# Taking care of missing data
-
-X_test[:,2] = ageImputerTrain.transform(X_test[:,2].reshape(-1,1)).flatten()
-
-# Encoding categorical data
-# Encoding the Independent Variable
-X_test[:,1] = sexEncoder.transform(X_test[:,1])
-X_test[:,6] = embarkedEncoder.transform(X_test[:,6])
-
-## Feature Scaling
-#X_test[:,2] = ageScale.transform(X_test[:,2].reshape(-1,1)).flatten()
 
 
 # Predicting the Test set results
-y_pred = classifier.predict(X_test)'''
+y_pred = classifier.predict(X_test)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
